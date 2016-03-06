d4bea12fd55b8b24f83180acc05e48d31c63a00e
Sigrid <sigridklerke@gmail.com>
2016-03-06 15:12:15 +0100

    working aoi-annotator (but slow)

--- etvec/annotator.py
+++ etvec/annotator.py
@@ -13,15 +13,43 @@ and an optional label column.
 # (i.e.: dundee:wordID,Line; tobii:x_pxls,y_pxls)
 
 
-def labeler(df, coord_df):
+def annotate_coords(df, coord_df, label=None):
     """
     Expects a dataframe (from a reader-function)
     + a dataframe for looking-up (X,Y)-coordinates
     based on stim-column.
-    Returns the reader-dataframe with a 'label' column.
+    Returns the reader-dataframe with aoi-based columns.
     """
 
-    #df.groupby('stim') = 
+    df['aoi'], df['aoi_id'] = None, None
 
-    #result = df
-    return #result
+    # go through each stimulus with groupby:
+    for stimulus, subdf in df.groupby('stim'):
+
+        # fetch relevant coordinates
+        coords = coord_df[coord_df.stim == stimulus]
+
+        # aoi_condition = ((coords.top < fix.coordY) &
+        #                  (coords.bottom > fix.coordY) &
+        #                  (coords.left > fix.coordX) &
+        #                  (coords.right < fix.coordX))
+
+        aoi = subdf.apply(lambda fix:
+                                  coords[((coords.top < fix.coordY) &
+                                          (coords.bottom > fix.coordY) &
+                                          (coords.left < fix.coordX) &
+                                          (coords.right > fix.coordX))].text.min(),
+                                  axis=1).T
+
+        aoi_id = subdf.apply(lambda fix:
+                                  coords[((coords.top < fix.coordY) &
+                                          (coords.bottom > fix.coordY) &
+                                          (coords.left < fix.coordX) &
+                                          (coords.right > fix.coordX))].id.min(),
+                                  axis=1).T
+
+        # insert the new col in the original df:
+        df.loc[subdf.index, ['aoi']] = aoi
+        df.loc[subdf.index, ['aoi_id']] = aoi_id
+
+    return df
