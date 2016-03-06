3f7d9b1a8e10d3a0740ba3e708038a7b6a12eb4f
Sigrid <sigridklerke@gmail.com>
2016-03-06 16:00:56 +0100

    aoi-annotator working with fixation counts.

--- etvec/annotator.py
+++ etvec/annotator.py
@@ -11,14 +11,18 @@ and an optional label column.
 # provide x-y resolution per stimulus of words
 # using the same unit as reader-coordinates
 # (i.e.: dundee:wordID,Line; tobii:x_pxls,y_pxls)
+import pandas as pd
 
 
-def annotate_coords(df, coord_df, label=None):
+def annotate_coords(df, coord_df, label=None, fixcount=True):
     """
     Expects a dataframe (from a reader-function)
     + a dataframe for looking-up (X,Y)-coordinates
     based on stim-column.
     Returns the reader-dataframe with aoi-based columns.
+    Label should be a df or nested dictionary with
+    [stimulus, tokenID] as keys and label as value.
+    Fixcount annotates how many times the aoi has been fixated incl. current.
     """
 
     df['aoi'], df['aoi_id'] = None, None
@@ -29,22 +33,34 @@ def annotate_coords(df, coord_df, label=None):
         # fetch relevant coordinates
         coords = coord_df[coord_df.stim == stimulus]
 
-        # aoi_condition = ((coords.top < fix.coordY) &
-        #                  (coords.bottom > fix.coordY) &
-        #                  (coords.left > fix.coordX) &
-        #                  (coords.right < fix.coordX))
-
+        # map fixation and aoi coordinates
         aoi_id = subdf.apply(lambda fix:
-                                  coords[((coords.top < fix.coordY) &
-                                          (coords.bottom > fix.coordY) &
-                                          (coords.left < fix.coordX) &
-                                          (coords.right > fix.coordX))].id.min(),
-                                  axis=1).T
+                             coords[((coords.top < fix.coordY) &
+                                     (coords.bottom > fix.coordY) &
+                                     (coords.left < fix.coordX) &
+                                     (coords.right > fix.coordX))].id.min(),
+                             axis=1).T
 
         aoi = aoi_id.apply(lambda x: coords[coords.id == x].text.min())
 
-        # insert the new col in the original df:
+        # insert the new columns in the original df:
         df.loc[subdf.index, ['aoi']] = aoi
         df.loc[subdf.index, ['aoi_id']] = aoi_id
 
+    df = df.drop(['coordX', 'coordY'], axis=1)
+
+    if fixcount:
+        df['fixcount'] = df.groupby(['stim', 'subj']).aoi_id.apply(lambda x:
+                                                                   fnummer(x))
+
     return df
+
+
+def fnummer(fix_seq):
+    """
+    helper function for annotating fixation counts.
+    """
+    fcount = []
+    for i, x in fix_seq.items():
+        fcount.append(len([v for v in fix_seq.values[:i] if v == x]))
+    return pd.Series(fcount, index=fix_seq.index)
